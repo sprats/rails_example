f783111f309fd8133a0221d0e61688e312faa1be
Sergey Vasiunin <s.vasiunin@gmail.com>
2016-09-06 21:13:15 +0700

    added the comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article
   include Mongoid::Document
   field :title, type: String
   field :text, type: String
+  has_many :comments
   validates :title, presence:true,
   					length: { minimum: 5 }
 end
