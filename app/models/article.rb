5d78f8eb95a7b03c76be5708104ab79466b3e574
Sergey Vasiunin <s.vasiunin@gmail.com>
2016-09-06 21:28:20 +0700

    added comments removal

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article
   include Mongoid::Document
   field :title, type: String
   field :text, type: String
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence:true,
   					length: { minimum: 5 }
 end
